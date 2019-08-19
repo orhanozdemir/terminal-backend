865b589b0f30a2a0203dc3719acb8f545122e9e9
Orhan Özdemir <ozdemirorhan95@gmail.com>
2019-08-19 11:41:25 +0300

    enum changes

--- src/main/java/com/procsin/API/Controller/ShopController.java
+++ src/main/java/com/procsin/API/Controller/ShopController.java
@@ -1,4 +1,24 @@
 package com.procsin.API.Controller;
 
+import com.procsin.API.Service.Implementation.ShopServiceImpl;
+import com.procsin.DB.Entity.Shop;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/shop")
 public class ShopController {
+
+    @Autowired
+    private ShopServiceImpl shopService;
+
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    public List<Shop> getShopList() {
+        return shopService.shopList();
+    }
+
 }
